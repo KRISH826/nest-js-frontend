34f5db0b37a6d4714c66821fd2358e8542324716
KRISH826 <krishnendupanja98@gmail.com>
2026-08-16 03:24:28 +0530

    feat: add LoginForm component with social auth and email credential inputs

--- components/auth/login/LoginForm.tsx
+++ components/auth/login/LoginForm.tsx
@@ -65,18 +65,6 @@ export function LoginForm({
                                 />
                             </Field>
                             <Field>
-                                <div className="flex items-center">
-                                    <FieldLabel htmlFor="password">Password</FieldLabel>
-                                    <a
-                                        href="#"
-                                        className="ml-auto text-sm underline-offset-4 hover:underline"
-                                    >
-                                        Forgot your password?
-                                    </a>
-                                </div>
-                                <Input id="password" type="password" required />
-                            </Field>
-                            <Field>
                                 <Button type="submit">Login</Button>
                                 <FieldDescription className="text-center">
                                     Don&apos;t have an account? <a href="#">Sign up</a>
